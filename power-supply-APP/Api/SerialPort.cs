f5f2c8638410c70c5cb2e53e3a261e2e9dea41eb
artur <artur251373@gmail.com>
2025-02-23 10:01:38 +0300

    Create SerialPort.cs

---
+++ power-supply-APP/Api/SerialPort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_supply_APP.Api
+{
+    using System;
+    using System.IO.Ports;
+
+    public interface IPortService
+    {
+        void Open();
+        void Close();
+        void WriteData(string data);
+    }
+
+    public class SerialPortService : IPortService
+    {
+        private readonly SerialPort _serialPort;
+
+        public SerialPortService(string portName, int baudRate)
+        {
+            _serialPort = new SerialPort(portName, baudRate);
+        }
+
+        public void Open() => _serialPort.Open();
+        public void Close() => _serialPort.Close();
+        public void WriteData(string data)
+        {
+            if (_serialPort.IsOpen)
+                _serialPort.WriteLine(data);
+        }
+    }
+
+}
