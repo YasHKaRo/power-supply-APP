b83d6322285a7c0ebff9e704230959578957cb51
artur <artur251373@gmail.com>
2025-02-26 15:58:17 +0300

    lets start tests

---
+++ power-supply-APP/Api/Modules/test1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_supply_APP.Api.Modules
+{
+    internal class test1
+    {
+    }
+}
